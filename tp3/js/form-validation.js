cbd781e4e136f62e4a02485b44ce73b0da574c8d
MeriemBekk <bekkouchemeriem@hotmail.com>
2022-11-28 09:40:26 +0100

    TP3

--- tp3/js/form-validation.js
+++ tp3/js/form-validation.js
@@ -8,21 +8,36 @@ window.onload = function () {   // ce code est exécuter une fois que toute la p
   }
 
 
-  let dateNaissance = new Date(2018, 8, 22); // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#syntaxe
-let dateNaissanceTimestamp = dateNaissance.getTime();
+let datedeNaissance = new Date(2018, 8, 22); // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#syntaxe
+let datedeNaissanceTimestamp = datedeNaissance.getTime();
 let nowTimestamp = Date.now()
 var myModal = new bootstrap.Modal(document.getElementById('myModal'));
 myModal.show();
-document.querySelector("#lname")
-document.querySelector("#lname").value;
-document.querySelector("#Date")
-document.querySelector("#Date").value;
+
+
+const lname = document.querySelector("#lname");
+const Date = document.querySelector("#Date");
+
 document.querySelector("#inputAddress")
 document.querySelector("#inputAddress").value;
 document.querySelector(".modal-title").textContent = "Bienvenue Meriem"
 document.querySelector(".modal-body").innerHTML = '<img src="https://maps.googleapis.com/maps/api/staticmap?markers=Paris&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/>'  
 
 
-
  
+function init() { 
+  console.log("init");
+
+ var myform = document.getElementById("#form");
+
+myform.addEventListener("submit", function(e) { console.log("submit");
+var username = document.getElementById("#lname").value;  var errors = '';
+          
+if(username == '') errors += 'Please specify a username.\n';
+
+if(errors != '') { alert(errors); e.preventDefault(); }
+
+}); };
+
+
 };
